de6972c7551c7efea46565d918b86805eb222025
JuanIrache <yrache@gmail.com>
2020-03-03 00:03:53 +0100

    attempt struct for json schema

--- utils/mgjson.go
+++ utils/mgjson.go
@@ -12,6 +12,51 @@ type SourceData struct {
 }
 
 type mgjson struct {
+	Version                string `json:"version"`
+	Creator                string `json:"creator"`
+	DynamicSamplesPresentB string `json:"dynamicSamplesPresentB"`
+	DynamicDataInfo        struct {
+		UseTimecodeB bool `json:"useTimecodeB"`
+		UtcInfo      struct {
+			PrecisionLength int  `json:"precisionLength"`
+			IsGMT           bool `json:"isGMT"`
+		} `json:"utcInfo"`
+	} `json:"dynamicDataInfo"`
+	DataOutline []struct {
+		ObjectType  string `json:"objectType"`
+		DisplayName string `json:"displayName"`
+		DampleSetID string `json:"sampleSetID"`
+		DataType    struct {
+			Type                   string `json:"type"`
+			NumberStringProperties struct {
+				Pattern struct {
+					DigitsInteger int `json:"digitsInteger"`
+					DigitsDecimal int `json:"digitsDecimal"`
+				} `json:"pattern"`
+				Range struct {
+					Occuring struct {
+						Min int `json:"min"`
+						Max int `json:"max"`
+					} `json:"occuring"`
+					Legal struct {
+						Min int `json:"min"`
+						Max int `json:"max"`
+					} `json:"legal"`
+				} `json:"range"`
+			} `json:"numberStringProperties"`
+		} `json:"dataType"`
+		Interpolation         string `json:"interpolation"`
+		HasExpectedFrequencyB bool   `json:"hasExpectedFrequecyB"`
+		SampleCount           int    `json:"sampleCount"`
+		MatchName             string `json:"matchName"`
+	} `json:"dataOutline"`
+	DataDynamicSamples []struct {
+		SampleSetID string `json:"sampleSetID"`
+		Samples     []struct {
+			Time  string `json:"time"`
+			Value string `json:"value"`
+		} `json:"samples"`
+	} `json:"dataDynamicSamples"`
 }
 
 func FormatMgjson(sd SourceData) mgjson {
