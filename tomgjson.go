2dbbb49176dc520aa753ea88976ba63b76036ed0
JuanIrache <yrache@gmail.com>
2020-03-05 21:46:43 +0100

    testing godoc docs

--- tomgjson.go
+++ tomgjson.go
@@ -1,28 +1,5 @@
 /*
-# tomgjson
-
-Experimenting with converting other time based data sources to [Adobe's mgJSON format](https://github.com/JuanIrache/mgjson)
-
-The first iteration will be very limited in terms of customization and supported formats, but the provided parser funcs can potentially be adapted to any source that associates time with values.
-
-For instructions on how to use mgJSON files in After Effects, see [Work with Data-driven animation](https://helpx.adobe.com/after-effects/using/data-driven-animations.html)
-
-## Sample project templates
-
-You can find sample After Effects projects that use mgJSON files on the [GoPro Telemetry Extractor page](https://goprotelemetryextractor.com). Look for the **Lite templates**.
-
-## Videos made with mgJSON
-
-- [mgJSON playlist on YouTube](https://www.youtube.com/playlist?list=PLgoeWSWqXedI7FbZccAEudt2_t8qPX0Px)
-
-If you create something with mgJSON, let me know and I'll add it to the list.
-
-## Software that supports mgJSON
-
-These apps can output mgJSON files:
-
-- [GoPro Telemetry Extractor](https://goprotelemetryextractor.com)
-- [DJI Telemetry Overlay](https://djitelemetryoverlay.com)
+Test documentation
 */
 
 package tomgjson
