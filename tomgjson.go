cbad3b1d640361e2a41843ee379c361596016d1e
JuanIrache <yrache@gmail.com>
2020-03-27 16:53:19 +0100

    utils was not generic utils, move funcs back to tomgjson

--- tomgjson.go
+++ tomgjson.go
@@ -3,10 +3,42 @@ package tomgjson
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math"
 	"strconv"
+	"strings"
 	"time"
 )
+
+// Like math.Max but with ints
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Returns both sides of a float number as strings
+func sides(n float64) (string, string) {
+	sides := strings.Split(strconv.FormatFloat(math.Abs(n), 'f', -1, 64), ".")
+	if len(sides) == 1 {
+		sides = append(sides, "0")
+	}
+	if len(sides) != 2 {
+		log.Panicf("Badly formatted float: %v %v", n, sides)
+	}
+	return sides[0], sides[1]
+}
+
+// Make sure float values are within mgJSON's valid values
+const largestMgjsonNum = 2147483648.0
+
+func validValue(v float64) float64 {
+	if math.IsNaN(v) {
+		return 0
+	}
+	return math.Max(math.Min(v, largestMgjsonNum), -largestMgjsonNum)
+}
 
 // Stream contains a slice of values or strings and their label
 // The slices must be of the same length as the timing slice in their parent's FormattedData
